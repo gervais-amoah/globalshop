0a8b10f630c856bf430951b2b7a2c418662f0780
Gervais Amoah <gervaisamoah@yahoo.com>
2023-10-02 11:31:55 +0000

    Add the Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 
 export default function App() {
   return (
     <>
       <Header />
-      <div>Welcome to General Shop!</div>
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to General Shop!</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 }
