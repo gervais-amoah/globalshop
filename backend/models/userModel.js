903e784fd6bdd76e347bb87eb290cca86289ec3a
Gervais Amoah <gervaisamoah@yahoo.com>
2023-10-03 10:47:57 +0000

    add "phoneNumber" field to user model

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -5,6 +5,7 @@ const userSchena = new mongoose.Schema(
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    phoneNumber: { type: String, required: true },
     isAdmin: { type: Boolean, required: true, default: false },
   },
   { timestamps: true }
