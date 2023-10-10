6d4e7f92506ccbef543459401777b7bcae2617ea
Gervais Amoah <gervaisamoah@yahoo.com>
2023-10-10 12:22:25 +0000

    User Register Endpoint & Passwd Encryption

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema(
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    phoneNumber: { type: String, required: true },
+    phoneNumber: { type: String, required: false },
     isAdmin: { type: Boolean, required: true, default: false },
   },
   { timestamps: true }
@@ -15,6 +15,13 @@ const userSchema = new mongoose.Schema(
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) next();
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 const User = mongoose.model("User", userSchema);
 
