e26dbab0a75664d0cfa001a46da9f6cda640fbae
Gervais Amoah <gervaisamoah@yahoo.com>
2023-10-11 14:52:04 +0000

    Creating an order (fron the front)

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -9,19 +9,19 @@ const oderSchema = new mongoose.Schema(
         qty: { type: Number, required: true },
         image: { type: String, required: true },
         price: { type: Number, required: true },
-        products: {
+        product: {
           type: mongoose.Schema.Types.ObjectId,
           required: true,
           ref: "Product",
         },
       },
     ],
+    paymentMethod: { type: String, required: true },
     shippingAddress: {
       address: { type: String, required: true },
       city: { type: String, required: true },
       postalCode: { type: String, required: true },
       country: { type: String, required: true },
-      paymentMethod: { type: String, required: true },
       paymentREsult: {
         id: { type: String },
         status: { type: String },
