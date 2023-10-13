e5d6e2cfd826b180e79d5750864130cd3966813f
Gervais Amoah <gervaisamoah@yahoo.com>
2023-10-13 13:11:48 +0000

    Multer & Image Upload Endpoint

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,9 @@ import connectDB from "./config/db.js";
 import orderRoutes from "./routes/orderRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
+
+import path from "path";
 
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
@@ -27,14 +30,20 @@ app.get("/", (req, res) => {
   res.send("API is up and running...");
 });
 
+// ROUTES
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/upload", uploadRoutes);
 
 // PAYPAL
 app.get("/api/config/paypal", (req, res) =>
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
 );
+
+//  IMAGE UPLOAD
+const __dirname = path.resolve(); // gets the current directory
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 app.use(notFound);
 app.use(errorHandler);
