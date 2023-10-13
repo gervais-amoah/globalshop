81b1956acb838dfa7dbad954bc9dd47523e19f57
Gervais Amoah <gervaisamoah@yahoo.com>
2023-10-13 19:20:43 +0000

    Createa Review - Backend

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,6 +5,7 @@ import {
   getProductById,
   getProducts,
   updateProduct,
+  createProductReview,
 } from "../controllers/productController.js";
 import { admin, protect } from "../middleware/authMiddleware.js";
 
@@ -16,5 +17,7 @@ router
   .get(getProductById)
   .put(protect, admin, updateProduct)
   .delete(protect, admin, deleteProduct);
+
+router.route("/:id/reviews").post(protect, createProductReview);
 
 export default router;
