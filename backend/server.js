1bc585364c5ad4681b199f113bd102753f35d15e
Gervais Amoah <gervaisamoah@yahoo.com>
2023-10-14 12:57:43 +0000

    production build set up

--- backend/server.js
+++ backend/server.js
@@ -26,10 +26,6 @@ app.use(express.urlencoded({ extended: true }));
 //  Cookie parser middleware
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.send("API is up and running...");
-});
-
 // ROUTES
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
@@ -44,6 +40,18 @@ app.get("/api/config/paypal", (req, res) =>
 //  IMAGE UPLOAD
 const __dirname = path.resolve(); // gets the current directory
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolveq(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is up and running...");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
